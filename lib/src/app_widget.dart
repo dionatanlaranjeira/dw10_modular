8c7a395d77768ed153d04c61209ba7d0bcefa12f
dionatanlaranjeira <dionatanribeiro.netrr@gmail.com>
2023-05-15 12:13:08 -0400

    inicio flutter web

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Modular.setInitialRoute('/home');
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Home Page',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
+  }
+}
